c9d4d743fde259a4bcf0e05b561796312e1f4155
Marc Marcos <marcmarcosmadruga@gmail.com>
2023-07-23 12:09:57 +0200

    Initial commit + recycled regenerateRoutes.py from another project

---
+++ regenerateRoutes.py
@@ -0,0 +1,24 @@
+import sqlite3
+import gpxpy
+import gpxpy.gpx
+import os
+
+DATABASE = 'database.db'
+
+conn = sqlite3.connect(DATABASE)
+cursor = conn.cursor()
+
+dir_path = 'backend/routes/'
+
+for path in os.listdir(dir_path):
+    if path.endswith('.gpx'):
+        print(path)
+        name = str(path).split('.')[0]
+
+        # If a route with the same path already exists, skip this one
+        if cursor.execute('SELECT * FROM routes WHERE route_path=?', (str(path),)).fetchone() is None:
+            cursor.execute('INSERT INTO routes (route_path, route_name, status) VALUES (?, ?, ?)', (str(path), name, 0))
+
+cursor.close()
+conn.commit()
+conn.close()
