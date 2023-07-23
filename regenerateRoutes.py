50c52f949928d8d69d90f53f0f16dfb3daf760de
Marc Marcos <marcmarcosmadruga@gmail.com>
2023-07-23 14:53:19 +0200

    Change way that regeneration is handled

--- regenerateRoutes.py
+++ regenerateRoutes.py
@@ -10,6 +10,11 @@ cursor = conn.cursor()
 
 dir_path = 'routes/'
 
+# If a route is in the database but not in the folder, delete it
+for row in cursor.execute('SELECT * FROM routes'):
+    if row[0] not in os.listdir(dir_path):
+        cursor.execute('DELETE FROM routes WHERE route_path=?', (row[0],))
+
 for path in os.listdir(dir_path):
     if path.endswith('.gpx'):
         print(path)
